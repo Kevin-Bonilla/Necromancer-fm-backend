f9ec26fb0f44a079257c7f4fbba6b917cc91788c
Kevin Bonilla <82667162+Kevin-Bonilla@users.noreply.github.com>
2026-03-25 10:29:02 -0500

    Initializing project

---
+++ src/main/java/com/KevinBonilla/Necromancer_fm/NecromancerFmApplication.java
@@ -0,0 +1,13 @@
+package com.KevinBonilla.Necromancer_fm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NecromancerFmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NecromancerFmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KevinBonilla/Necromancer_fm/NecromancerFmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KevinBonilla.Necromancer_fm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NecromancerFmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
