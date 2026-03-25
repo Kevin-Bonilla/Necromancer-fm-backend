7a8c6b2ee43528ba90d18f57384a8f95346f9091
Kevin Bonilla <82667162+Kevin-Bonilla@users.noreply.github.com>
2026-03-25 17:01:54 -0500

    Artist entity made

---
+++ src/main/java/com/KevinBonilla/Necromancer_fm/entities/Artist.java
@@ -0,0 +1,15 @@
+package com.KevinBonilla.Necromancer_fm.entities;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Artist {
+    //TODO: refine this later
+    // loosely writing this and will probably refine later
+
+    private String artistName;
+}
