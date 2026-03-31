879c87e99485d55ecc86cca21a37894c21877648
Kevin Bonilla <82667162+Kevin-Bonilla@users.noreply.github.com>
2026-03-31 10:51:40 -0500

    configuring h2 db through application.properties instead of in the test file

--- src/test/java/com/KevinBonilla/Necromancer_fm/NecromancerFmApplicationTests.java
+++ src/test/java/com/KevinBonilla/Necromancer_fm/NecromancerFmApplicationTests.java
@@ -3,13 +3,7 @@ package com.KevinBonilla.Necromancer_fm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-		"spring.datasource.url=jdbc:h2:mem:testdb",
-		"spring.datasource.driver-class-name=org.h2.Driver",
-		"spring.datasource.username=sa",
-		"spring.datasource.password=",
-		"spring.jpa.hibernate.ddl-auto=create-drop"
-})
+@SpringBootTest
 class NecromancerFmApplicationTests {
 
 	@Test
