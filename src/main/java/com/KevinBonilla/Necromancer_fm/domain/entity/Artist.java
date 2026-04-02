0c9bf2e79bac4162630ca7ef5f2bc2f77dd8573d
Kevin Bonilla <82667162+Kevin-Bonilla@users.noreply.github.com>
2026-04-01 21:56:15 -0500

    artist constructors, getters, and setters

--- src/main/java/com/KevinBonilla/Necromancer_fm/domain/entity/Artist.java
+++ src/main/java/com/KevinBonilla/Necromancer_fm/domain/entity/Artist.java
@@ -31,4 +31,64 @@ public class Artist {
 
     @Column(name = "updated_at", nullable = false)
     private Instant updatedAt;
+
+    public Artist() {
+    }
+
+    public Artist(UUID id, String artistName, int playcount, ArtistStatus artistStatus, Instant createdAt, Instant updatedAt) {
+        this.id = id;
+        this.artistName = artistName;
+        this.playcount = playcount;
+        this.artistStatus = artistStatus;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public int getPlaycount() {
+        return playcount;
+    }
+
+    public void setPlaycount(int playcount) {
+        this.playcount = playcount;
+    }
+
+    public ArtistStatus getArtistStatus() {
+        return artistStatus;
+    }
+
+    public void setArtistStatus(ArtistStatus artistStatus) {
+        this.artistStatus = artistStatus;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
