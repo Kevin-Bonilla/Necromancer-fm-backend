49335595644a207971b3e1d77b4722951f494b08
Kevin Bonilla <82667162+Kevin-Bonilla@users.noreply.github.com>
2026-04-08 22:29:06 -0500

    created ArtistController

---
+++ src/main/java/com/KevinBonilla/Necromancer_fm/controller/ArtistController.java
@@ -0,0 +1,9 @@
+package com.KevinBonilla.Necromancer_fm.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/artists")
+public class ArtistController {
+}
