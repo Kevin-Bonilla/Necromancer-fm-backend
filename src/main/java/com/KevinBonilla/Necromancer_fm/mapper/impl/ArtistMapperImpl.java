f6bc0c28bb5681172951e29dcd816cde10806fe4
Kevin Bonilla <82667162+Kevin-Bonilla@users.noreply.github.com>
2026-04-08 22:16:29 -0500

    function implementations

--- src/main/java/com/KevinBonilla/Necromancer_fm/mapper/impl/ArtistMapperImpl.java
+++ src/main/java/com/KevinBonilla/Necromancer_fm/mapper/impl/ArtistMapperImpl.java
@@ -4,15 +4,26 @@ import com.KevinBonilla.Necromancer_fm.domain.CreateArtistRequest;
 import com.KevinBonilla.Necromancer_fm.domain.dto.ArtistDto;
 import com.KevinBonilla.Necromancer_fm.domain.entity.Artist;
 import com.KevinBonilla.Necromancer_fm.mapper.ArtistMapper;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ArtistMapperImpl implements ArtistMapper {
     @Override
     public CreateArtistRequest fromDto(CreateArtistRequest dto) {
-        return null;
+        return new CreateArtistRequest(
+                dto.artistName(),
+                dto.playcount(),
+                dto.artistStatus()
+        );
     }
 
     @Override
     public ArtistDto toDto(Artist artist) {
-        return null;
+        return new ArtistDto(
+                artist.getId(),
+                artist.getArtistName(),
+                artist.getPlaycount(),
+                artist.getArtistStatus()
+        );
     }
 }
